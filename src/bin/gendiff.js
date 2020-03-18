1ed00a0f2a329b6ba66c47a91cb5ce5be57bf70c
matiush-tejn <matiush-tejn@yandex.ru>
2020-03-18 21:14:36 +0500

    bind to gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 import program from 'commander';
 import { version, description } from '../../package.json';
+import gendiff from '..';
 
 program
   .version(version)
   .description(description)
   .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
-  .action((firstConfig, secondConfig) => console.log(firstConfig, secondConfig));
-
-program.parse(process.argv);
+  .action((firstConfig, secondConfig) => console.log(gendiff(firstConfig, secondConfig)))
+  .parse(process.argv);
