5e8192670cd42f9b4450bfe70f3f5677fcfc8b09
matiush-tejn <matiush-tejn@yandex.ru>
2020-03-19 21:00:13 +0500

    rename fixtures by standard

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,8 +2,8 @@ import { readFileSync } from 'fs';
 import gendiff from '../src';
 
 test('calculating the difference', () => {
-  const filepath1 = 'fixtures/before.json';
-  const filepath2 = 'fixtures/after.json';
-  const diff = readFileSync('fixtures/diff', 'utf-8');
+  const filepath1 = '__fixtures__/before.json';
+  const filepath2 = '__fixtures__/after.json';
+  const diff = readFileSync('__fixtures__/diff', 'utf-8');
   expect(gendiff(filepath1, filepath2)).toBe(diff);
 });
