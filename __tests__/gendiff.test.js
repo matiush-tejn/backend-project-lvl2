c21bac0c9f5d4a2a80f9bd5944c36cd013c36b22
matiush-tejn <matiush-tejn@yandex.ru>
2020-03-20 18:16:29 +0500

    refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,16 +1,12 @@
 import { readFileSync } from 'fs';
 import gendiff from '../src';
 
-const diff = readFileSync('__fixtures__/diff', 'utf-8');
+const getPath = (filename) => (`__fixtures__/${filename}`);
+const diff = readFileSync(getPath('diff'), 'utf-8');
 
-test('difference of json-files', () => {
-  const filepath1 = '__fixtures__/before.json';
-  const filepath2 = '__fixtures__/after.json';
-  expect(gendiff(filepath1, filepath2)).toBe(diff);
-});
-
-test('difference of yaml-files', () => {
-  const filepath1 = '__fixtures__/before.yml';
-  const filepath2 = '__fixtures__/after.yml';
-  expect(gendiff(filepath1, filepath2)).toBe(diff);
+test.each([
+  ['before.json', 'after.json'],
+  ['before.yml', 'after.yml'],
+])('gendiff(%s, %s)', (first, second) => {
+  expect(gendiff(getPath(first), getPath(second))).toBe(diff);
 });
