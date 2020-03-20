e81100fd353560b18f0b6e1237eaeaa1223d4e14
matiush-tejn <matiush-tejn@yandex.ru>
2020-03-20 17:01:34 +0500

    add test for yaml-files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,16 @@
 import { readFileSync } from 'fs';
 import gendiff from '../src';
 
-test('calculating the difference', () => {
+test('difference of json-files', () => {
   const filepath1 = '__fixtures__/before.json';
   const filepath2 = '__fixtures__/after.json';
+  const diff = readFileSync('__fixtures__/diff', 'utf-8');
+  expect(gendiff(filepath1, filepath2)).toBe(diff);
+});
+
+test('difference of yaml-files', () => {
+  const filepath1 = '__fixtures__/before.yml';
+  const filepath2 = '__fixtures__/after.yml';
   const diff = readFileSync('__fixtures__/diff', 'utf-8');
   expect(gendiff(filepath1, filepath2)).toBe(diff);
 });
