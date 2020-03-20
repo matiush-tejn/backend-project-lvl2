8b572f82a2822d9ba6f473a5c7e07b7c4bebdd55
matiush-tejn <matiush-tejn@yandex.ru>
2020-03-20 20:26:14 +0500

    refactoring

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,27 +1,28 @@
 import { readFileSync } from 'fs';
 import { extname } from 'path';
 import { has } from 'lodash';
-import { safeLoad } from 'js-yaml';
+import parser from './parse';
 
-const compare = (obj1, obj2) => {
-  const changed = Object.entries(obj1).reduce((acc, [key, value]) => {
-    if (!has(obj2, key)) return { ...acc, [`- ${key}`]: value };
-    if (value === obj2[key]) return { ...acc, [`  ${key}`]: value };
-    return { ...acc, [`+ ${key}`]: value, [`- ${key}`]: obj2[key] };
+const compare = (data1, data2) => {
+  const changed = Object.entries(data1).reduce((acc, [key, value]) => {
+    if (!has(data2, key)) return { ...acc, [`- ${key}`]: value };
+    if (value === data2[key]) return { ...acc, [`  ${key}`]: value };
+    return { ...acc, [`+ ${key}`]: value, [`- ${key}`]: data2[key] };
   }, {});
-  const added = Object.entries(obj2).reduce((acc, [key, value]) => (
-    has(obj1, key) ? acc : { ...acc, [`+ ${key}`]: value }), {});
+  const added = Object.entries(data2).reduce((acc, [key, value]) => (
+    has(data1, key) ? acc : { ...acc, [`+ ${key}`]: value }), {});
   const result = { ...changed, ...added };
   return JSON.stringify(result, null, ' ').split('').filter((char) => char !== '"').join('');
 };
 
+const getParsedData = (filepath) => {
+  const inputData = readFileSync(filepath, 'utf-8');
+  const ext = extname(filepath);
+  return parser(ext, inputData);
+};
+
 export default (filepath1, filepath2) => {
-  const data1 = readFileSync(filepath1, 'utf-8');
-  const data2 = readFileSync(filepath2, 'utf-8');
-  const ext1 = extname(filepath1);
-  const ext2 = extname(filepath2);
-  const obj1 = ext1 === '.json' ? JSON.parse(data1) : safeLoad(data1);
-  const obj2 = ext2 === '.json' ? JSON.parse(data2) : safeLoad(data2);
-  const diff = compare(obj1, obj2);
-  return diff;
+  const parsedData1 = getParsedData(filepath1);
+  const parsedData2 = getParsedData(filepath2);
+  return compare(parsedData1, parsedData2);
 };
