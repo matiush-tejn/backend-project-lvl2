a648f35bf15e92e1763edd6f8495ec92a1248221
matiush-tejn <matiush-tejn@yandex.ru>
2020-03-21 13:59:25 +0500

    add ini parsing

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,6 +7,7 @@ const diff = readFileSync(getPath('diff'), 'utf-8');
 test.each([
   ['before.json', 'after.json'],
   ['before.yml', 'after.yml'],
+  ['before.ini', 'after.ini'],
 ])('gendiff(%s, %s)', (first, second) => {
   expect(gendiff(getPath(first), getPath(second))).toBe(diff);
 });
--- src/parse.js
+++ src/parse.js
@@ -1,10 +1,12 @@
 import { readFileSync } from 'fs';
 import { extname } from 'path';
 import { safeLoad } from 'js-yaml';
+import { parse } from 'ini';
 
 const parsers = {
   '.json': JSON.parse,
   '.yml': safeLoad,
+  '.ini': parse,
 };
 
 export default (filepath) => {
