91fbcf89addb7352d1219b7d08646db0b62f6da5
matiush-tejn <matiush-tejn@yandex.ru>
2020-03-23 19:52:57 +0500

    separate processes for creating intermediate views and rendering have been introduced

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,13 +1,22 @@
 import { has, union } from 'lodash';
 import getParsedData from './parse';
 
-const compare = (data1, data2) => {
+const buildNodes = (data1, data2) => {
   const keys = union(Object.keys(data1), Object.keys(data2));
-  const result = keys.reduce((acc, key) => {
-    if (has(data1, key) && !has(data2, key)) return { ...acc, [`- ${key}`]: data1[key] };
-    if (!has(data1, key) && has(data2, key)) return { ...acc, [`+ ${key}`]: data2[key] };
-    if (data1[key] === data2[key]) return { ...acc, [`  ${key}`]: data1[key] };
-    return { ...acc, [`- ${key}`]: data1[key], [`+ ${key}`]: data2[key] };
+  return keys.map((key) => {
+    if (has(data1, key) && !has(data2, key)) return { type: 'deleted', key, value: data1[key] };
+    if (!has(data1, key) && has(data2, key)) return { type: 'added', key, value: data2[key] };
+    if (data1[key] === data2[key]) return { type: 'equal', key, value: data1[key] };
+    return { type: 'changed', key, value: [data1[key], data2[key]] };
+  });
+};
+
+const renderer = (nodes) => {
+  const result = nodes.reduce((acc, { type, key, value }) => {
+    if (type === 'deleted') return { ...acc, [`- ${key}`]: value };
+    if (type === 'added') return { ...acc, [`+ ${key}`]: value };
+    if (type === 'equal') return { ...acc, [`  ${key}`]: value };
+    return { ...acc, [`- ${key}`]: value[0], [`+ ${key}`]: value[1] };
   }, {});
   return JSON.stringify(result, null, ' ').split('').filter((char) => char !== '"').join('');
 };
@@ -15,5 +24,6 @@ const compare = (data1, data2) => {
 export default (filepath1, filepath2) => {
   const parsedData1 = getParsedData(filepath1);
   const parsedData2 = getParsedData(filepath2);
-  return compare(parsedData1, parsedData2);
+  const nodes = buildNodes(parsedData1, parsedData2);
+  return renderer(nodes);
 };
