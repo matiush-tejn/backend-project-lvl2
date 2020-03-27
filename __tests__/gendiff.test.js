a87d4be607be0b062ee7661d79049413a4c53c19
matiush-tejn <matiush-tejn@yandex.ru>
2020-03-27 16:35:31 +0500

    update tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,6 +3,7 @@ import gendiff from '../src';
 
 const getPath = (filename) => (`__fixtures__/${filename}`);
 const flatDiff = readFileSync(getPath('flat/diff'), 'utf-8');
+const nestedDiff = readFileSync(getPath('nested/diff'), 'utf-8');
 
 test.each([
   ['flat/before.json', 'flat/after.json'],
@@ -10,4 +11,12 @@ test.each([
   ['flat/before.ini', 'flat/after.ini'],
 ])('gendiff(%s, %s)', (first, second) => {
   expect(gendiff(getPath(first), getPath(second))).toBe(flatDiff);
+});
+
+test.each([
+  ['nested/before.json', 'nested/after.json'],
+  ['nested/before.yml', 'nested/after.yml'],
+  ['nested/before.ini', 'nested/after.ini'],
+])('gendiff(%s, %s)', (first, second) => {
+  expect(gendiff(getPath(first), getPath(second))).toBe(nestedDiff);
 });
