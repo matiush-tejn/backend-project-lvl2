beecb2a0f22de6a11a06b3e76c0b5ede45884851
matiush-tejn <matiush-tejn@yandex.ru>
2020-03-27 17:14:35 +0500

    remove flat structure tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,21 +2,12 @@ import { readFileSync } from 'fs';
 import gendiff from '../src';
 
 const getPath = (filename) => (`__fixtures__/${filename}`);
-const flatDiff = readFileSync(getPath('flat/diff'), 'utf-8');
-const nestedDiff = readFileSync(getPath('nested/diff'), 'utf-8');
+const nestedDiff = readFileSync(getPath('diff'), 'utf-8');
 
 test.each([
-  ['flat/before.json', 'flat/after.json'],
-  ['flat/before.yml', 'flat/after.yml'],
-  ['flat/before.ini', 'flat/after.ini'],
-])('gendiff(%s, %s)', (first, second) => {
-  expect(gendiff(getPath(first), getPath(second))).toBe(flatDiff);
-});
-
-test.each([
-  ['nested/before.json', 'nested/after.json'],
-  ['nested/before.yml', 'nested/after.yml'],
-  ['nested/before.ini', 'nested/after.ini'],
+  ['before.json', 'after.json'],
+  ['before.yml', 'after.yml'],
+  ['before.ini', 'after.ini'],
 ])('gendiff(%s, %s)', (first, second) => {
   expect(gendiff(getPath(first), getPath(second))).toBe(nestedDiff);
 });
