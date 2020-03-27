8237dfd49ed37c17dd43d867b26611116f0c1fb3
matiush-tejn <matiush-tejn@yandex.ru>
2020-03-27 16:47:56 +0500

    add processing of nested structures

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,21 +2,26 @@ import { has, union } from 'lodash';
 import getParsedData from './parse';
 import renderer from './renderer';
 
-const buildDiffNodes = (data1, data2) => {
+const buildAst = (data1, data2) => {
   const predicates = {
     deleted: (key) => has(data1, key) && !has(data2, key),
     added: (key) => !has(data1, key) && has(data2, key),
+    nested: (key) => data1[key] instanceof Object && data2[key] instanceof Object,
     equal: (key) => data1[key] === data2[key],
     changed: (key) => data1[key] !== data2[key],
   };
   const getType = (key) => Object.keys(predicates).find((type) => predicates[type](key));
   const keys = union(Object.keys(data1), Object.keys(data2));
-  return keys.map((key) => [getType(key), key, data1[key], data2[key]]);
+  return keys.map((key) => {
+    const type = getType(key);
+    const children = type === 'nested' ? buildAst(data1[key], data2[key]) : [];
+    return [type, key, data1[key], data2[key], children];
+  });
 };
 
 export default (filepath1, filepath2) => {
   const parsedData1 = getParsedData(filepath1);
   const parsedData2 = getParsedData(filepath2);
-  const diffNodes = buildDiffNodes(parsedData1, parsedData2);
-  return renderer(diffNodes);
+  const astTree = buildAst(parsedData1, parsedData2);
+  return renderer(astTree);
 };
--- src/renderer.js
+++ src/renderer.js
@@ -1,9 +1,16 @@
-export default (diffNodes) => {
-  const result = diffNodes.reduce((acc, [type, key, firstValue, secondValue]) => {
-    if (type === 'deleted') return { ...acc, [`- ${key}`]: firstValue };
-    if (type === 'added') return { ...acc, [`+ ${key}`]: secondValue };
+import { mapKeys } from 'lodash';
+
+const align = (content) => (
+  content instanceof Object ? mapKeys(content, (value, key) => `  ${key}`) : content);
+
+const normalization = (astTree) => astTree
+  .reduce((acc, [type, key, firstValue, secondValue, children]) => {
+    if (type === 'deleted') return { ...acc, [`- ${key}`]: align(firstValue) };
+    if (type === 'added') return { ...acc, [`+ ${key}`]: align(secondValue) };
+    if (type === 'nested') return { ...acc, [`  ${key}`]: normalization(children) };
     if (type === 'equal') return { ...acc, [`  ${key}`]: firstValue };
-    return { ...acc, [`- ${key}`]: firstValue, [`+ ${key}`]: secondValue };
+    return { ...acc, [`- ${key}`]: align(firstValue), [`+ ${key}`]: align(secondValue) };
   }, {});
-  return JSON.stringify(result, null, ' ').split('').filter((char) => char !== '"').join('');
-};
+
+export default (astTree) => JSON.stringify(normalization(astTree), null, 4)
+  .split('').filter((char) => char !== '"' && char !== ',').join('');
