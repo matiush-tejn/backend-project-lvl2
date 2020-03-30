eaf42f8ac4b06fc19fbaba01b29e524480761ece
matiush-tejn <matiush-tejn@yandex.ru>
2020-03-30 12:58:22 +0500

    refactoring

--- src/renderer.js
+++ src/renderer.js
@@ -3,14 +3,14 @@ import { mapKeys } from 'lodash';
 const align = (content) => (
   content instanceof Object ? mapKeys(content, (value, key) => `  ${key}`) : content);
 
-const normalization = (astTree) => astTree.reduce((acc, { type, key, data }) => {
+const convert = (astTree) => astTree.reduce((acc, { type, key, data }) => {
   if (type === 'deleted') return { ...acc, [`- ${key}`]: align(data) };
   if (type === 'added') return { ...acc, [`+ ${key}`]: align(data) };
-  if (type === 'nested') return { ...acc, [`  ${key}`]: normalization(data) };
+  if (type === 'nested') return { ...acc, [`  ${key}`]: convert(data) };
   if (type === 'equal') return { ...acc, [`  ${key}`]: data };
   return { ...acc, [`- ${key}`]: align(data[0]), [`+ ${key}`]: align(data[1]) };
 }, {});
 
 const spacesCount = 4;
-export default (astTree) => JSON.stringify(normalization(astTree), null, spacesCount)
+export default (astTree) => JSON.stringify(convert(astTree), null, spacesCount)
   .split('').filter((char) => char !== '"' && char !== ',').join('');
