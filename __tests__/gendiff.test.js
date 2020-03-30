00d94cb4be438cb10e0214f13e2dd2b9094b1645
matiush-tejn <matiush-tejn@yandex.ru>
2020-03-30 21:35:54 +0500

    rename 'nestedDiff' -> 'diff'

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,12 +2,12 @@ import { readFileSync } from 'fs';
 import gendiff from '../src';
 
 const getPath = (filename) => (`__fixtures__/${filename}`);
-const nestedDiff = readFileSync(getPath('diff'), 'utf-8');
+const diff = readFileSync(getPath('diff'), 'utf-8');
 
 test.each([
   ['before.json', 'after.json'],
   ['before.yml', 'after.yml'],
   ['before.ini', 'after.ini'],
 ])('gendiff(%s, %s)', (first, second) => {
-  expect(gendiff(getPath(first), getPath(second))).toBe(nestedDiff);
+  expect(gendiff(getPath(first), getPath(second))).toBe(diff);
 });
