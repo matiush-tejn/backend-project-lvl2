5ed8c1688123151b99238d02829f051c3d1c4f5f
matiush-tejn <matiush-tejn@yandex.ru>
2020-03-30 12:52:04 +0500

    refactoring

--- src/buildAst.js
+++ src/buildAst.js
@@ -1,37 +1,38 @@
-import { has, union } from 'lodash';
+import { has, union, identity } from 'lodash';
+
+const nodeTypes = [
+  {
+    type: 'deleted',
+    predicate: (key, data1, data2) => has(data1, key) && !has(data2, key),
+    getData: identity,
+  },
+  {
+    type: 'added',
+    predicate: (key, data1, data2) => !has(data1, key) && has(data2, key),
+    getData: (undef, value) => value,
+  },
+  {
+    type: 'nested',
+    predicate: (key, data1, data2) => data1[key] instanceof Object && data2[key] instanceof Object,
+    getData: (nested1, nested2, buildAst) => buildAst(nested1, nested2),
+  },
+  {
+    type: 'equal',
+    predicate: (key, data1, data2) => data1[key] === data2[key],
+    getData: identity,
+  },
+  {
+    type: 'changed',
+    predicate: (key, data1, data2) => data1[key] !== data2[key],
+    getData: (value1, value2) => [value1, value2],
+  },
+];
 
 const buildAst = (data1, data2) => {
-  const nodeTypes = [
-    {
-      type: 'deleted',
-      predicate: (key) => has(data1, key) && !has(data2, key),
-      getData: (key) => data1[key],
-    },
-    {
-      type: 'added',
-      predicate: (key) => !has(data1, key) && has(data2, key),
-      getData: (key) => data2[key],
-    },
-    {
-      type: 'nested',
-      predicate: (key) => data1[key] instanceof Object && data2[key] instanceof Object,
-      getData: (key) => buildAst(data1[key], data2[key]),
-    },
-    {
-      type: 'equal',
-      predicate: (key) => data1[key] === data2[key],
-      getData: (key) => data1[key],
-    },
-    {
-      type: 'changed',
-      predicate: (key) => data1[key] !== data2[key],
-      getData: (key) => [data1[key], data2[key]],
-    },
-  ];
   const keys = union(Object.keys(data1), Object.keys(data2));
   return keys.map((key) => {
-    const { type, getData } = nodeTypes.find(({ predicate }) => predicate(key));
-    return { type, key, data: getData(key) };
+    const { type, getData } = nodeTypes.find(({ predicate }) => predicate(key, data1, data2));
+    return { type, key, data: getData(data1[key], data2[key], buildAst) };
   });
 };
 
