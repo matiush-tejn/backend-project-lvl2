2fad78cd4a941c11c0beb8b4a5c53091780c56b8
matiush-tejn <matiush-tejn@yandex.ru>
2020-04-01 10:50:23 +0500

    added output of deep nested structures

--- src/renderer.js
+++ src/renderer.js
@@ -1,23 +1,23 @@
 const indentStep = 4;
 const getIndent = (count) => ' '.repeat(count * indentStep);
+const addBrackets = (content, indentCount) => `{\n${content}\n${getIndent(indentCount)}  }`;
+
+const stringify = (value, indentCount) => {
+  if (!(value instanceof Object)) return value.toString();
+  const content = Object.keys(value)
+    .map((key) => `${getIndent(indentCount + 1)}  ${key}: ${stringify(value[key], indentCount + 1)}`)
+    .join('\n');
+  return addBrackets(content, indentCount);
+};
 
 const renderer = (astTree, indentCount) => {
   const indent = getIndent(indentCount);
-  const addBrackets = (content) => `{\n${content}\n${indent}  }`;
 
-  const stringify = (value) => {
-    if (!(value instanceof Object)) return value.toString();
-    const content = Object.keys(value)
-      .map((key) => `${getIndent(indentCount + 1)}  ${key}: ${value[key]}`)
-      .join('\n');
-    return addBrackets(content);
-  };
-
-  const buildString = (prefix, key, value) => `${indent}${prefix} ${key}: ${stringify(value)}`;
+  const buildString = (prefix, key, value) => `${indent}${prefix} ${key}: ${stringify(value, indentCount)}`;
   const handlers = {
     deleted: (key, value) => buildString('-', key, value),
     added: (key, value) => buildString('+', key, value),
-    nested: (key, value) => buildString(' ', key, addBrackets(renderer(value, indentCount + 1))),
+    nested: (key, value) => buildString(' ', key, addBrackets(renderer(value, indentCount + 1), indentCount)),
     equal: (key, value) => buildString(' ', key, value),
     changed: (key, [value1, value2]) => `${buildString('-', key, value1)}\n${buildString('+', key, value2)}`,
   };
