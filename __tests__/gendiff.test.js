d09a1d4e4ea717ffceb9a29830e29a43b2a5ca5a
matiush-tejn <matiush-tejn@yandex.ru>
2020-04-02 13:11:36 +0500

    implemented output in json format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,14 +4,14 @@ import gendiff from '../src';
 const getPath = (filename) => (`__fixtures__/${filename}`);
 const treelikeDiff = readFileSync(getPath('treelike-diff'), 'utf-8');
 const plainDiff = readFileSync(getPath('plain-diff'), 'utf-8');
+const jsonDiff = readFileSync(getPath('json-diff'), 'utf-8');
 
 test.each([
-  ['before.json', 'after.json', 'treelike', treelikeDiff],
-  ['before.yml', 'after.yml', 'treelike', treelikeDiff],
-  ['before.ini', 'after.ini', 'treelike', treelikeDiff],
-  ['before.json', 'after.json', 'plain', plainDiff],
-  ['before.yml', 'after.yml', 'plain', plainDiff],
-  ['before.ini', 'after.ini', 'plain', plainDiff],
-])('gendiff(%s, %s, %s)', (first, second, format, expected) => {
-  expect(gendiff(getPath(first), getPath(second), format)).toBe(expected);
+  ['treelike', treelikeDiff],
+  ['plain', plainDiff],
+  ['json', jsonDiff],
+])('%s output', (format, expected) => {
+  expect(gendiff(getPath('before.json'), getPath('after.json'), format)).toBe(expected);
+  expect(gendiff(getPath('before.yml'), getPath('after.yml'), format)).toBe(expected);
+  expect(gendiff(getPath('before.ini'), getPath('after.ini'), format)).toBe(expected);
 });
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,6 +1,7 @@
 import treelike from './treelike';
 import plain from './plain';
+import json from './json';
 
-const formatters = { treelike, plain };
+const formatters = { treelike, plain, json };
 export const formats = Object.keys(formatters);
 export default formatters;
