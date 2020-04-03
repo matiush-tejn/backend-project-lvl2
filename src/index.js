5404f990e10ebaaee4c3ec4f92afb205ae4f5be1
matiush-tejn <matiush-tejn@yandex.ru>
2020-04-03 15:45:29 +0500

    action with side effects moved from the parser module to the central module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import getParsedData from './parse';
+import { readFileSync } from 'fs';
+import { extname } from 'path';
+import parse from './parsers';
 import buildAst from './buildAst';
 import formatters from './formatters';
 
+const getParsedData = (filepath) => {
+  const data = readFileSync(filepath, 'utf-8');
+  const ext = extname(filepath);
+  return parse(data, ext);
+};
+
 export default (filepath1, filepath2, format) => {
-  const parsedData1 = getParsedData(filepath1);
-  const parsedData2 = getParsedData(filepath2);
-  const astTree = buildAst(parsedData1, parsedData2);
+  const oldData = getParsedData(filepath1);
+  const newData = getParsedData(filepath2);
+  const astTree = buildAst(oldData, newData);
   const formatter = formatters[format];
   return formatter(astTree);
 };
---
+++ src/parsers.js
@@ -0,0 +1,10 @@
+import { safeLoad as yamlParse } from 'js-yaml';
+import { decode as iniParse } from 'ini';
+
+const parsers = {
+  '.json': JSON.parse,
+  '.yml': yamlParse,
+  '.ini': iniParse,
+};
+
+export default (data, ext) => parsers[ext](data);
